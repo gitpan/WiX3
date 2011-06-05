ecbda0f8d0ed3c02cf3b6095eedeb50ac5419499
Curtis Jewell <csjewell@cpan.org>
2011-06-05 16:18:34 +0000

    Import of CSJEWELL/WiX3-0.011 from CPAN.
    gitpan-cpan-distribution: WiX3
    gitpan-cpan-version:      0.011
    gitpan-cpan-path:         CSJEWELL/WiX3-0.011.tar.gz
    gitpan-cpan-author:       CSJEWELL
    gitpan-cpan-maturity:     released

--- lib/WiX3/Util/StrictConstructor.pm
+++ lib/WiX3/Util/StrictConstructor.pm
@@ -2,21 +2,18 @@ package                                # Hide from PAUSE.
   WiX3::Util::StrictConstructor;
 
 # Corresponds to MooseX::StrictConstructor.
+# 0.010004 = MX::SC 0.16
 
-use 5.008001;
-use Moose 0.94 qw();
+use 5.008003;
+use Moose 2.00 qw();
 use Moose::Exporter;
 use Moose::Util::MetaRole;
-use WiX3::Util::Role::StrictConstructor;
-use WiX3::Util::Role::StrictConstructorMeta;
 
-our $VERSION = '0.010';
-$VERSION =~ s/_//ms;
+#use WiX3::Util::Trait::StrictConstructor::Class;
 
-Moose::Exporter->setup_import_methods(
-	class_metaroles =>
-	  { constructor => ['WiX3::Util::Role::StrictConstructorMeta'] },
-	base_class_roles => ['WiX3::Util::Role::StrictConstructor'],
-);
+our $VERSION = '0.011';
+
+Moose::Exporter->setup_import_methods( class_metaroles =>
+	  { class => ['WiX3::Util::Trait::StrictConstructor::Class'], }, );
 
 1;
